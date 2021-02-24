412a1cf33bcb63fd8ea23e8765b0e8185e683a2c
Louis Tsai <louis993546@gmail.com>
2021-02-24 21:54:03 +0100

    Basic photo fetching

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,15 +15,21 @@
  */
 package com.example.androiddevchallenge
 
+import android.app.Application
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.ui.theme.MyTheme
+import dev.chrisbanes.accompanist.glide.GlideImage
+import dev.chrisbanes.accompanist.imageloading.ImageLoadState
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,11 +42,44 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
+
+@Composable
+fun AnimalCard(
+    modifier: Modifier = Modifier,
+    animalPhotoUrl: String,
+    animalPhotoContentDescription: String,
+) {
+    GlideImage(modifier = modifier, data = animalPhotoUrl) { state ->
+        when (state) {
+            ImageLoadState.Empty -> { }
+            ImageLoadState.Loading -> Text("Spinner")
+            is ImageLoadState.Success -> Image(
+                painter = state.painter,
+                contentDescription = animalPhotoContentDescription
+            )
+            is ImageLoadState.Error -> {
+                Timber.e(state.throwable)
+                Text("🙈")
+            }
+        }
+    }
+}
+
 // Start building your app here!
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+//        Text(text = "Ready... Set... GO!")
+        AnimalCard(
+            animalPhotoUrl = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+            animalPhotoContentDescription = "TBD"
+        )
     }
 }
 
