f037caf835d186d3aee1cafde044550d446833de
Louis Tsai <louis993546@gmail.com>
2021-02-24 22:34:46 +0100

    List of cards

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -15,20 +15,25 @@
  */
 package com.example.androiddevchallenge
 
-import android.app.Application
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androiddevchallenge.ui.theme.MyTheme
 import dev.chrisbanes.accompanist.glide.GlideImage
-import dev.chrisbanes.accompanist.imageloading.ImageLoadState
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -42,30 +47,45 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        Timber.plant(Timber.DebugTree())
-    }
-}
+data class Animal(
+    val name: String,
+    val image: String,
+)
 
+// TODO how to give "fix" size to image?
 @Composable
 fun AnimalCard(
     modifier: Modifier = Modifier,
     animalPhotoUrl: String,
     animalPhotoContentDescription: String,
 ) {
-    GlideImage(modifier = modifier, data = animalPhotoUrl) { state ->
-        when (state) {
-            ImageLoadState.Empty -> { }
-            ImageLoadState.Loading -> Text("Spinner")
-            is ImageLoadState.Success -> Image(
-                painter = state.painter,
-                contentDescription = animalPhotoContentDescription
-            )
-            is ImageLoadState.Error -> {
-                Timber.e(state.throwable)
-                Text("🙈")
+    GlideImage(
+        modifier = modifier.aspectRatio(1f),
+        data = animalPhotoUrl,
+        contentDescription = animalPhotoContentDescription,
+        contentScale = ContentScale.Crop,
+        fadeIn = true,
+        error = { e ->
+            Timber.e(e.throwable)
+            Text("🙅‍")
+        }
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ToBeAdoptList(
+    modifier: Modifier = Modifier,
+    animals: List<Animal>,
+) {
+    LazyVerticalGrid(modifier = modifier, cells = GridCells.Fixed(2)) {
+        items(animals) { animal ->
+            Column {
+                Text(animal.name)
+                AnimalCard(
+                    animalPhotoUrl = animal.image,
+                    animalPhotoContentDescription = animal.name
+                )
             }
         }
     }
@@ -75,10 +95,45 @@ fun AnimalCard(
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
-//        Text(text = "Ready... Set... GO!")
-        AnimalCard(
-            animalPhotoUrl = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
-            animalPhotoContentDescription = "TBD"
+        ToBeAdoptList(
+            animals = listOf(
+                Animal(
+                    name = "TBD 1",
+                    image = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+                ),
+                Animal(
+                    name = "TBD 2",
+                    image = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+                ),
+                Animal(
+                    name = "TBD 3",
+                    image = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+                ),
+                Animal(
+                    name = "TBD 4",
+                    image = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+                ),
+                Animal(
+                    name = "TBD 5",
+                    image = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+                ),
+                Animal(
+                    name = "TBD 6",
+                    image = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+                ),
+                Animal(
+                    name = "TBD 7",
+                    image = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+                ),
+                Animal(
+                    name = "TBD 8",
+                    image = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+                ),
+                Animal(
+                    name = "TBD 9",
+                    image = "https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515",
+                )
+            )
         )
     }
 }
